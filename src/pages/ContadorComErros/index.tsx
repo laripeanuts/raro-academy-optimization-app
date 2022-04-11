1f9da8f3d4ed46e4b2567ee8e977582f8bc4f553
Paulo Fernandes <phfernandespereira@gmail.com>
2022-04-11 18:15:44 -0300

    adiciona tela de contador com erros

---
+++ src/pages/ContadorComErros/index.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { Button } from "../../components/Button";
+
+const Error: React.FC = ({ children }) => {
+  return (
+    <h1 className="text-center text-red-500 text-base font-medium">
+      { children }
+    </h1>
+  )
+}
+
+export const ContadorComErros = () => {
+  const [contador, setContador] = useState(0);
+  const [erros, setErros] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (contador < 0) {
+      setErros([
+        'Contador não pode ser menor que zero',
+        ...erros,
+      ]);
+    }
+
+    if (contador > 10) {
+      setErros([
+        'Contador não pode ser maior que dez',
+        ...erros,
+      ]);
+    }
+  }, [contador]);
+
+  const incrementa = () => {
+    setContador(contador + 1);
+  }
+  
+  const decrementa = () => {
+    setContador(contador - 1);
+  }
+
+  return (
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow-2xl sm:rounded-lg sm:px-10">
+          <h1 className="text-center text-gray-500 text-2xl font-medium uppercase">
+            { contador }
+          </h1>
+          <div className="mt-2">
+            <div className="flex justify-between space-x-2">
+              <Button onClick={ decrementa }>{ `<` }</Button>
+              <Button onClick={ incrementa }>{ `>` }</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        {
+          erros.map(error => (
+            <Error key={error}>{ error }</Error>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
